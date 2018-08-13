9be593829ebd3a3918a658f49adda6d3339b6b5f
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2018-08-13 16:38:43 -0700

    init

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "google" {}
+
+resource "google_compute_network" "network" {
+  name                    = "${var.cluster_name}-network"
+  auto_create_subnetworks = "false"
+}
+
+## Two subnetworks (masters, agents)
+resource "google_compute_subnetwork" "master-subnet" {
+  name          = "${var.cluster_name}-master-subnet"
+  ip_cidr_range = "${var.master_cidr_range}"
+  network       = "${google_compute_network.network.self_link}"
+  region        = "${var.gcp_region}"
+}
+
+resource "google_compute_subnetwork" "agent-subnet" {
+  name          = "${var.cluster_name}-agent-subnet"
+  ip_cidr_range = "${var.agent_cidr_range}"
+  network       = "${google_compute_network.network.self_link}"
+  region        = "${var.gcp_region}"
+}
