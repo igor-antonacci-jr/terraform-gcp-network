274bb79b896a59b4ee35ea93ebc8c2358f60c2df
Lukas Loesche <lukas@mesosphere.io>
2018-09-27 16:55:59 +0200

    Automatic update of variable descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,24 @@
 # Cluster Name
 output "name_prefix" {
+  description = "Cluster Name"
   value = "${var.name_prefix}"
 }
 
 # Master CIDR Range
 output "master_cidr_range" {
+  description = "master cidr range"
   value = "${var.master_cidr_range}"
 }
 
 # Master Subnetwork Name
 output "master_subnetwork_name" {
+  description = "master subnetwork name"
   value = "${google_compute_subnetwork.master-subnet.name}"
 }
 
 # Agent Subnetwork Name
 output "agent_subnetwork_name" {
+  description = "agent subnetwork name"
   value = "${google_compute_subnetwork.agent-subnet.name}"
 }
 
--- variables.tf
+++ variables.tf
@@ -1,8 +1,14 @@
 # Cluster Name
-variable "name_prefix" {}
+variable "name_prefix" {
+  description = "Cluster Name"
+}
 
 # Master CIDR Range
-variable "master_cidr_range" {}
+variable "master_cidr_range" {
+  description = "master cidr range"
+}
 
 # Agent CIDR Range
-variable "agent_cidr_range" {}
+variable "agent_cidr_range" {
+  description = "agent cidr range"
+}
